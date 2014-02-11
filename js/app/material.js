4b2e9472a98891abff946fa8acdff298e4420aab
Felix Palmer <felixpalmer@gmail.com>
2014-02-11 12:06:58 +0100

    Update README and add #include sample code

--- js/app/material.js
+++ js/app/material.js
@@ -1,4 +1,7 @@
 define( ["three", "shader!simple.vert", "shader!simple.frag", "texture"], function ( THREE, simpleVert, simpleFrag, texture ) {
+  // Shader objects support redefining of #defines.
+  // See `simple.frag` file, where `faceColor` is already defined to be white, and we are overriding it to red here
+  simpleFrag.define( "faceColor", "vec3(1.0, 0, 0)" );
   return {
     bump: new THREE.MeshPhongMaterial( { bumpMap: texture.grass } ),
     grass: new THREE.MeshBasicMaterial( { map: texture.grass } ),
